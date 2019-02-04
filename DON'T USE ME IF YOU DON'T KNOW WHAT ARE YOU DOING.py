bcb3576caf6f287b5c277a71f00e9efa4ce73734
OnionMaster03 <47319097+OnionMaster03@users.noreply.github.com>
2019-02-04 13:17:40 +0100

    Add files via upload

--- DON'T USE ME IF YOU DON'T KNOW WHAT ARE YOU DOING.py
+++ DON'T USE ME IF YOU DON'T KNOW WHAT ARE YOU DOING.py
@@ -1,3 +1,7 @@
 import pickle
-db = {"db":["158163241.dates"]}
+import time
+now = time.localtime()
+bd = {"chat_id" : "INSERT HERE YOUR CHAT ID", "1" : {"giorno" : now[2], "mese" : now [1], "nome" : "UNNAMED"}}
+pickle.dump(bd, open("INSERT HERE YOUR CHAT ID" + ".dates", "wb"))
+db = {"db":["INSERT HERE YOUR CHAT ID.dates"]}
 pickle.dump(db, open("database.db", "wb"))
